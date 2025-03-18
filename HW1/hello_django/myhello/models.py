48455b0f6c9d0bd36b1564b4eefb3c76d777581a
John20050201 <151489373+John20050201@users.noreply.github.com>
2025-03-18 14:27:24 +0800

    Add files via upload

---
+++ HW1/hello_django/myhello/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class Post(models.Model):
+    Department = models.CharField(max_length=100)
+    CourseTitle = models.CharField(max_length=100)
+    Instructor = models.CharField(max_length=100)
+    #content = models.TextField(blank=True)
+    #photo = models.URLField(blank=True)
+    #location = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+from django.db import models
+
+class Post(models.Model):
+    Department = models.CharField(max_length=100)
+    CourseTitle = models.CharField(max_length=100)
+    Instructor = models.CharField(max_length=100)
+    #content = models.TextField(blank=True)
+    #photo = models.URLField(blank=True)
+    #location = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
